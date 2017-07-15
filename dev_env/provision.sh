eab8cf019cafb401a5c213f141f5444d49b9dbbd
Dave Simons <simonsdave@gmail.com>
2017-07-15 06:29:48 -0400

    fix dev env matplotlib provisioning error

--- dev_env/provision.sh
+++ dev_env/provision.sh
@@ -40,24 +40,18 @@ apt-get update -y && apt-get install -y python-virtualenv python python-dev pyth
 # as per http://blog.pangyanhan.com/posts/2015-07-25-how-to-install-matplotlib-using-virtualenv-on-ubuntu.html
 apt-get -y build-dep matplotlib
 
-# as per http://stackoverflow.com/questions/29073802/matplotlib-cannot-find-configuration-file-matplotlibrc
-mkdir -p /root/.config/matplotlib
+su vagrant <<'EOF'
+mkdir -p ~/.config/matplotlib
 
-su root <<'EOF'
-echo 'set ruler' > /root/.config/matplotlib/matplotlibrc
-echo 'set hlsearch' >> /root/.config/matplotlib/matplotlibrc
-
-echo '# see http://matplotlib.org/users/customizing.html#the-matplotlibrc-file' >> /root/.config/matplotlib/matplotlibrc
-echo '# for general details on this file' >> /root/.config/matplotlib/matplotlibrc
-
-echo '# the backend configuration option was added (the whole reason this file' >> /root/.config/matplotlib/matplotlibrc
-echo '# was created actually) so that matplotlib could generate PNGs in a headless' >> /root/.config/matplotlib/matplotlibrc
-echo '# Ubuntu 14.04 deployment as per the info in this article' >> /root/.config/matplotlib/matplotlibrc
-echo '# http://stackoverflow.com/questions/2801882/generating-a-png-with-matplotlib-when-display-is-undefined' >> /root/.config/matplotlib/matplotlibrc
-echo 'backend : Agg' >> /root/.config/matplotlib/matplotlibrc
+echo '# see http://matplotlib.org/users/customizing.html#the-matplotlibrc-file' > ~/.config/matplotlib/matplotlibrc
+echo '# for general details on this file' >> ~/.config/matplotlib/matplotlibrc
+echo '' >> ~/.config/matplotlib/matplotlibrc
+echo '# the backend configuration option was added (the whole reason this file' >> ~/.config/matplotlib/matplotlibrc
+echo '# was created actually) so that matplotlib could generate PNGs in a headless' >> ~/.config/matplotlib/matplotlibrc
+echo '# Ubuntu 14.04 deployment as per the info in this article' >> ~/.config/matplotlib/matplotlibrc
+echo '# http://stackoverflow.com/questions/2801882/generating-a-png-with-matplotlib-when-display-is-undefined' >> ~/.config/matplotlib/matplotlibrc
+echo 'backend : Agg' >> ~/.config/matplotlib/matplotlibrc
 EOF
-
-chown --recursive root:root /root/.config
 
 #
 # assumes we're working in EST ...
