54b8e8477268f203fcb8d8da214da4c5b7e8cbb4
Dave Simons <simonsdave@gmail.com>
2018-06-15 23:43:26 -0400

    numpy 1.14.4 -> 1.14.5

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     ],
     install_requires=[
         'matplotlib==2.2.2',
-        'numpy==1.14.4',
+        'numpy==1.14.5',
         'python-dateutil==2.7.3',
     ],
     include_package_data=True,
