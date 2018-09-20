d2d96ea9d85fe4943d81235eadcf41c52a78846e
Dave Simons <simonsdave@gmail.com>
2018-09-19 23:26:27 -0400

    matplotlib 2.2.3 -> 3.0.0

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         'bin/analyze_restful_api_load_test_results.sh',
     ],
     install_requires=[
-        'matplotlib==2.2.3',
+        'matplotlib==3.0.0',
         'numpy==1.15.1',
         'python-dateutil==2.7.3',
     ],
