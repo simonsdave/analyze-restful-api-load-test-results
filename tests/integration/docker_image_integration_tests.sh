7593fce7ccbb085a16bbd359a27e84720751048e
simonsdave <simonsdave@gmail.com>
2019-01-22 07:00:29 -0500

    improve docker_image_integration_tests.sh checks

--- tests/integration/docker_image_integration_tests.sh
+++ tests/integration/docker_image_integration_tests.sh
@@ -7,17 +7,25 @@ SCRIPT_DIR_NAME="$( cd "$( dirname "$0" )" && pwd )"
 test_analyze_no_graphs() {
     DOCKER_IMAGE=${1:-}
 
+    STDOUT=$(mktemp)
+
     docker run \
         --rm \
         -i \
         "$DOCKER_IMAGE" \
         analyze_restful_api_load_test_results.sh \
-        < "$SCRIPT_DIR_NAME/stdin/happy_path.tsv"
+        < "$SCRIPT_DIR_NAME/data/happy_path_stdin.tsv" \
+        > "$STDOUT"
+
+    diff "$STDOUT" "$SCRIPT_DIR_NAME/data/happy_path_stdout.txt"
+
+    rm "$STDOUT"
 }
 
 test_analyze_with_graphs() {
     DOCKER_IMAGE=${1:-}
 
+    STDOUT=$(mktemp)
     GRAPH=$(mktemp)
 
     docker run \
@@ -27,11 +35,17 @@ test_analyze_with_graphs() {
         "$DOCKER_IMAGE" \
         analyze_restful_api_load_test_results.sh \
         "--graphs=/graphs/$(basename "$GRAPH")" \
-        < "$SCRIPT_DIR_NAME/stdin/happy_path.tsv"
+        < "$SCRIPT_DIR_NAME/data/happy_path_stdin.tsv" \
+        > "$STDOUT"
+
+    diff "$STDOUT" "$SCRIPT_DIR_NAME/data/happy_path_stdout.txt"
 
     # :TRICKY" implict check that pdf was created - rm will fail
     # and thus this script will fail if the pdf isn't created
+    # :TODO: how do we know the graph is a pdf and accurate?
     rm "$GRAPH"
+
+    rm "$STDOUT"
 }
 
 if [ $# != 1 ]; then
