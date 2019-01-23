19ab1b92371b0d477b001f7c04cdfb6889e39368
simonsdave <simonsdave@gmail.com>
2019-01-22 21:06:45 -0500

    move to build env in docker container

---
+++ dev_env/build-docker-image.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+#
+# This script builds a docker image which packages up the
+# development environment and associated tooling.
+#
+
+set -e
+
+SCRIPT_DIR_NAME="$( cd "$( dirname "$0" )" && pwd )"
+
+if [ $# != 1 ]; then
+    echo "usage: $(basename "$0") <docker image name>" >&2
+    exit 1
+fi
+
+DOCKER_IMAGE=${1:-}
+
+CONTEXT_DIR=$(mktemp -d 2> /dev/null || mktemp -d -t DAS)
+PROJECT_HOME_DIR="$SCRIPT_DIR_NAME/.."
+cp "$PROJECT_HOME_DIR/requirements.txt" "$CONTEXT_DIR/."
+cp "$PROJECT_HOME_DIR/setup.py" "$CONTEXT_DIR/."
+mkdir "$CONTEXT_DIR/analyze_restful_api_load_test_results"
+cp "$PROJECT_HOME_DIR/analyze_restful_api_load_test_results/__init__.py" "$CONTEXT_DIR/analyze_restful_api_load_test_results/."
+mkdir "$CONTEXT_DIR/bin"
+cp "$PROJECT_HOME_DIR/bin/analyze_restful_api_load_test_results.py" "$CONTEXT_DIR/bin/."
+cp "$PROJECT_HOME_DIR/bin/analyze_restful_api_load_test_results.sh" "$CONTEXT_DIR/bin/."
+
+cp "$SCRIPT_DIR_NAME/matplotlibrc" "$CONTEXT_DIR/."
+
+DEV_ENV_VERSION=$(cat "$SCRIPT_DIR_NAME/dev-env-version.txt")
+if [ "${DEV_ENV_VERSION:-}" == "master" ]; then
+    DEV_ENV_VERSION=latest
+fi
+
+TEMP_DOCKERFILE=$CONTEXT_DIR/Dockerfile
+cp "$SCRIPT_DIR_NAME/Dockerfile.template" "$TEMP_DOCKERFILE"
+sed \
+    -i \
+    -e "s|%DEV_ENV_VERSION%|$DEV_ENV_VERSION|g" \
+    "$TEMP_DOCKERFILE"
+
+docker build \
+    -t "$DOCKER_IMAGE" \
+    --file "$TEMP_DOCKERFILE" \
+    "$CONTEXT_DIR"
+
+rm -rf "$CONTEXT_DIR"
+
+exit 0
--- dev_env/provision.sh
+++ dev_env/provision.sh
@@ -6,34 +6,4 @@
 
 set -e
 
-#
-# install matplotlib
-#
-apt-get update -y
-apt-get install -y \
-    python-virtualenv \
-    python \
-    python-dev \
-    python-pip \
-    libxft-dev \
-    libfreetype6 \
-    libfreetype6-dev \
-    libffi-dev
-
-# as per http://blog.pangyanhan.com/posts/2015-07-25-how-to-install-matplotlib-using-virtualenv-on-ubuntu.html
-apt-get -y build-dep matplotlib
-
-su vagrant <<'EOF'
-mkdir -p ~/.config/matplotlib
-
-echo '# see http://matplotlib.org/users/customizing.html#the-matplotlibrc-file' > ~/.config/matplotlib/matplotlibrc
-echo '# for general details on this file' >> ~/.config/matplotlib/matplotlibrc
-echo '' >> ~/.config/matplotlib/matplotlibrc
-echo '# the backend configuration option was added (the whole reason this file' >> ~/.config/matplotlib/matplotlibrc
-echo '# was created actually) so that matplotlib could generate PNGs in a headless' >> ~/.config/matplotlib/matplotlibrc
-echo '# Ubuntu 14.04 deployment as per the info in this article' >> ~/.config/matplotlib/matplotlibrc
-echo '# http://stackoverflow.com/questions/2801882/generating-a-png-with-matplotlib-when-display-is-undefined' >> ~/.config/matplotlib/matplotlibrc
-echo 'backend : Agg' >> ~/.config/matplotlib/matplotlibrc
-EOF
-
 exit 0
