fe9d43819859f10a330a6a29c1d655d995c7c949
Dave Simons <simonsdave@gmail.com>
2019-12-29 19:44:44 -0500

    fix: some shell linting

--- tests/integration/docker-image-integration-tests.sh
+++ tests/integration/docker-image-integration-tests.sh
@@ -5,43 +5,49 @@ set -e
 SCRIPT_DIR_NAME="$( cd "$( dirname "$0" )" && pwd )"
 
 test_analyze_no_graphs() {
-    STDOUT=$(mktemp)
+    # :ODD: Normally you'd expect the line below to be something like
+    # "STDOUT=$(mktemp)" but when that was used the error "The path /var/<something>
+    # is not shared from OS X and is not known to Docker" was generated
+    # and could not figure out what the problem and hence the current
+    # implementation.
+    STDOUT=${SCRIPT_DIR_NAME}/stdout.txt
 
     docker run \
         --rm \
         -i \
-        "$DOCKER_IMAGE" \
+        "${DOCKER_IMAGE}" \
         analyze-restful-api-load-test-results.sh \
-        < "$SCRIPT_DIR_NAME/data/happy-path-stdin.tsv" \
-        > "$STDOUT"
+        < "${SCRIPT_DIR_NAME}/data/happy-path-stdin.tsv" \
+        > "${STDOUT}"
 
-    diff "$STDOUT" "$SCRIPT_DIR_NAME/data/happy-path-stdout.txt"
+    diff "${STDOUT}" "${SCRIPT_DIR_NAME}/data/happy-path-stdout.txt"
 
-    rm "$STDOUT"
+    rm -f "${STDOUT}"
 }
 
 test_analyze_with_graphs() {
-    STDOUT=$(mktemp)
-    GRAPH=$(mktemp)
+    # :ODD: see above :ODD: notes
+    STDOUT=${SCRIPT_DIR_NAME}/stdout.txt
+    GRAPH=${SCRIPT_DIR_NAME}/graph.pdf
 
     docker run \
         --rm \
         -i \
-        -v "$(dirname "$GRAPH"):/graphs" \
-        "$DOCKER_IMAGE" \
+        -v "$(dirname "${GRAPH}"):/graphs" \
+        "${DOCKER_IMAGE}" \
         analyze-restful-api-load-test-results.sh \
-        "--graphs=/graphs/$(basename "$GRAPH")" \
-        < "$SCRIPT_DIR_NAME/data/happy-path-stdin.tsv" \
-        > "$STDOUT"
+        "--graphs=/graphs/$(basename "${GRAPH}")" \
+        < "${SCRIPT_DIR_NAME}/data/happy-path-stdin.tsv" \
+        > "${STDOUT}"
 
-    diff "$STDOUT" "$SCRIPT_DIR_NAME/data/happy-path-stdout.txt"
+    diff "${STDOUT}" "${SCRIPT_DIR_NAME}/data/happy-path-stdout.txt"
 
-    # :TRICKY" implict check that pdf was created - rm will fail
+    # :TRICKY: implict check that pdf was created - rm will fail
     # and thus this script will fail if the pdf isn't created
     # :TODO: how do we know the graph is a pdf and accurate?
-    rm "$GRAPH"
+    rm -f "${GRAPH}"
 
-    rm "$STDOUT"
+    rm -f "${STDOUT}"
 }
 
 test_wrapper() {
@@ -62,6 +68,6 @@ NUMBER_TESTS_RUN=0
 test_wrapper test_analyze_no_graphs
 test_wrapper test_analyze_with_graphs
 echo ""
-echo "Successfully completed $NUMBER_TESTS_RUN integration tests."
+echo "Successfully completed ${NUMBER_TESTS_RUN} integration tests."
 
 exit 0
