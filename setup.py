26fe9d39f3cee5ce6881387d7f627db1f3f68529
Dave Simons <simonsdave@gmail.com>
2021-01-28 20:56:14 -0500

    feat: matplotlib 3.3.3 -> 3.3.4

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         'bin/analyze-restful-api-load-test-results.sh',
     ],
     install_requires=[
-        'matplotlib==3.3.3',
+        'matplotlib==3.3.4',
         'numpy==1.19.5',
         'python-dateutil>=2.8,<2.9',
     ],
