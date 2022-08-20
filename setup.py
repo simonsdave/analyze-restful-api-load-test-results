9ed249350807cf22e999efc1ee56a98c0b630e54
Dave Simons <simonsdave@gmail.com>
2022-08-20 08:48:05 -0400

    feat: numpy 1.22.3 -> 1.23.2

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     ],
     install_requires=[
         'matplotlib==3.5.3',
-        'numpy==1.22.3',
+        'numpy==1.23.2',
         'python-dateutil>=2.8,<2.9',
     ],
     include_package_data=True,
